29be05427eac4ddb6a7ddc0efee0344f1267f73c
Jananima28 <138774035+Jananima28@users.noreply.github.com>
2026-02-14 23:23:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import joblib
 import os
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load models (from project folder)
 classification_model = joblib.load('classification_model.joblib')
