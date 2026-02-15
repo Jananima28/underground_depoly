cdb0989cc78abc25b8a162e733109109bf1b92d8
Jananima28 <138774035+Jananima28@users.noreply.github.com>
2026-02-15 20:53:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import joblib
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)   
 CORS(app)
 
 # Load models
@@ -23,16 +23,13 @@ def predict():
 
         required_features = ['Voltage (V)', 'Current (A)', 'Temperature (C)', 'Moisture (%)']
 
-        # Check missing fields
         for feature in required_features:
             if feature not in data:
                 return jsonify({"error": f"Missing feature: {feature}"}), 400
 
-        # Create dataframe
         features_df = pd.DataFrame([[data[f] for f in required_features]],
                                    columns=required_features)
 
-        # Predictions
         predicted_fault_encoded = classification_model.predict(features_df)
         predicted_fault = label_encoder.inverse_transform(predicted_fault_encoded)[0]
         predicted_location = regression_model.predict(features_df)[0]
@@ -45,6 +42,10 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+
+if __name__ == "__main__":  
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port
 
 if _name_ == "_main_":
     port = int(os.environ.get("PORT", 10000))
